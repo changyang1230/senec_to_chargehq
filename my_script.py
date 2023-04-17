169c9240cf33199eb491bbadeeaa93dd2e557a40
changyang1230 <130973841+changyang1230@users.noreply.github.com>
2023-04-17 16:50:18 +0800

    Create my_script.py

---
+++ my_script.py
@@ -0,0 +1,49 @@
+"""
+PLEASE replace the SENEC IP and your API in this file.
+"""
+
+import time
+import json
+import requests
+from senec import Senec
+
+#init api
+api = Senec("{REPLACE WITH SENEC IP}")
+if not api:
+  sys.exit
+
+while True:
+    try:
+        # Get fresh data
+        data = api.get_values()
+        
+        # Extract the values and change the units to kW
+        production_kw = data["ENERGY"]['GUI_INVERTER_POWER']/1000
+        battery_discharge_kw = data["ENERGY"]['GUI_BAT_DATA_POWER'] / (-1000)
+        consumption_kw = data["ENERGY"]['GUI_HOUSE_POW']/1000
+        net_import_kw = data["ENERGY"]['GUI_GRID_POW']/1000
+        battery_soc = data["ENERGY"]['GUI_BAT_DATA_FUEL_CHARGE'] / 100
+
+        # Create the JSON payload
+        payload = {
+            "apiKey": "{REPLACE WITH YOUR OWN API}",
+            "siteMeters": {
+                "production_kw": production_kw,
+                "battery_discharge_kw": battery_discharge_kw,
+                "consumption_kw": consumption_kw,
+                "net_import_kw": net_import_kw,
+                "battery_soc": battery_soc
+            }
+        }
+
+        # Send the JSON payload to the API endpoint
+        response = requests.post("https://api.chargehq.net/api/public/push-solar-data", json=payload)
+        print(response.text)
+    except Exception as e:
+        print(e)
+
+    # Wait for 30 seconds before repeating (Charge HQ API limit)
+    time.sleep(30)
+
+# Close the driver
+driver.quit()
